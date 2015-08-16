40f56d6a0fec2b5ad2ab0b781d465fc81e3bbab2
Maxim Xu <xujialv@gmail.com>
2015-08-16 18:40:12 +0800

    a

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function() {
+
+'use strict';
+
+$('body').on('click', function(){
+   $('.container').css("visibility", "visible").addClass('animated fadeIn');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
