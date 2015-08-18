c2d21909006de77b6ca07c623ab724f50bcce1bb
Maxim Xu <xujialv@gmail.com>
2015-08-19 01:11:58 +0800

    a

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ $(function() {
 
 'use strict';
  
+setTimeout(function(){
+
+ $('i.fa-star').css('visibility', 'visible');
+
+}, 5000)
 
 setTimeout(function(){
 $('.promo nav').fadeIn(500).css('display', 'inline-block');
 }, 3750)
+
 
 $('body').on('click', 'a.btn', function(){
    $('.promo').hide();
@@ -13,6 +19,9 @@ $('body').on('click', 'a.btn', function(){
    setInterval(function(){
    $('.des').toggleClass("pulse animatedDelayed")
 }, 3000)
+
+
+
 });
 
 
