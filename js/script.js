1409ca57d26aa493844b8d8291ab3b230c0dd98a
Maxim Xu <xujialv@gmail.com>
2015-08-19 00:46:46 +0800

    a

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $('.promo nav').fadeIn(500).css('display', 'inline-block');
 
 $('body').on('click', 'a.btn', function(){
    $('.promo').hide();
-   $('.container').css('visibility', 'visible').addClass('animated fadeIn');
+   $('.container, .logoContainer').css('visibility', 'visible').addClass('animated fadeIn');
    setInterval(function(){
    $('.des').toggleClass("pulse animatedDelayed")
 }, 3000)
