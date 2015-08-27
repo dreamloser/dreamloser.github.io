4ee0d4af2fd3bf559a7fdd70568c2b51022b79b2
Maxim Xu <xujialv@gmail.com>
2015-08-27 21:52:58 +0800

    remove video on mobile devices, add flexbox css frame

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ $(function() {
 
 'use strict';  
 
+if(document.body.clientWidth <= 500) {
+    $('#player, #tubular-shield, .play, #skip').remove();
+    $('.logoContainer').css({'display':'block','visibility':'visible'});
+    $('.mask').css('background-color','rgb(226,40,78)');
+} else {
+	$('#player').tubular(); 
+}
+
 $('#skip').fadeIn(5000)
 
 $("#skip").click(function(){ 
         $('.play, .tex, #skip, #pg-container, #pg-player').hide();
-        $('.logoContainer').fadeIn(1000);
+        $('.logoContainer').css('visibility','visible').hide().fadeIn(1000);
         $('.mask').css('background-color','rgb(226,40,78)');
 }); 
  
